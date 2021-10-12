88518dae69675d96a9c7fe08ed747d630468c73e
nazzong <86649352+nazzong@users.noreply.github.com>
2021-10-12 10:37:15 +0900

    b

--- config/config.js
+++ config/config.js
@@ -2,25 +2,25 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 module.exports = {
-    development : {
-        username : process.env.DB_USER,
-        password : process.env.DB_PASS,
-        database : process.env.DB_NAME,
-        host : process.env.DB_HOST,
-        dialect : "mysql",
-    },
-    test : {
-        username : process.env.DB_USER,
-        password : process.env.DB_PASS,
-        database : process.env.DB_NAME,
-        host : process.env.DB_HOST,
-        dialect : "mysql",
-    },
-    prod : {
-        username : process.env.DB_USER,
-        password : process.env.DB_PASS,
-        database : process.env.DB_NAME,
-        host : process.env.DB_HOST,
-        dialect : "mysql",
-    },
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+  },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+  },
 };
